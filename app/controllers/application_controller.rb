9282c195df47284f29fcc642d3edd8158750ffb7
David Veytsman <dveyts@yahoo.com>
2017-10-24 13:44:57 -0400

    started working on the initial backend '

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
+
+helper_method :current_user, :logged_in?
+
+private
+
+def current_user
+  return nil unless session[:session_token]
+  @current_user ||= User.find_by(session_token: session[:session_token])
+  # see the benchbnb had to stop and do something else
