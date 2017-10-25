f81b8be68b3170ef952f97e3800dc3751347de7b
David Veytsman <dveyts@yahoo.com>
2017-10-25 18:49:45 -0400

    took out broken code

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import GreetingContainer from './greeting/greeting_container';
-import LandingContainer from './greeting/landing_container';
 import {Route, Link, Switch, HashRouter } from 'react-router-dom';
 import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
