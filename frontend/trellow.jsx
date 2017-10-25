61fba043725bf600afe85ad272e9a21fe94936c2
David Veytsman <dveyts@yahoo.com>
2017-10-25 11:49:40 -0400

    Tracked down the backend auth bug

--- frontend/trellow.jsx
+++ frontend/trellow.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
 // import configureStore from './store/store';
-
+import { signup, login, logout } from './util/session_api_util.js';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById('root');
+  window.login = login;
+  window.signup = signup;
+  window.logout = logout;
   ReactDOM.render(<div>Welcome to Trellow</div>, root );
 });
