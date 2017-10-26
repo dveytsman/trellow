6073f096acb946a9ee9a28c940370cb820ec3ae2
David Veytsman <dveyts@yahoo.com>
2017-10-26 09:37:06 -0400

    removed email from params

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -13,6 +13,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :email)
+    params.require(:user).permit(:username, :password)
   end
 end
