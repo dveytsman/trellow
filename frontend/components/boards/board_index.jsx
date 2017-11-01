8b1f6801db204b9ede343d4e343b74d293709bcf
David Veytsman <dveyts@yahoo.com>
2017-11-01 11:51:09 -0400

    got the create button functional but only in the guest user

--- frontend/components/boards/board_index.jsx
+++ frontend/components/boards/board_index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {fetchBoards, createBoard} from '../../actions/board_actions';
-
+import CreateBoardContainer from './create_board_container';
 class BoardIndex extends React.Component{
   constructor(props){
     super(props);
@@ -15,7 +15,12 @@ class BoardIndex extends React.Component{
     if(Object.keys(this.props.boards).length === 0){
       return null;
     }else {
-      return this.props.boards.map(board => <li key={board.id}>{board.title}</li>);
+      return(
+        <ul>
+          {this.props.boards.map(board => <li key={board.id}>{board.title}</li>)}
+          <CreateBoardContainer />
+        </ul>
+      );
     }
   }
 }
--- frontend/components/boards/create_board.jsx
+++ frontend/components/boards/create_board.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
-
+import createBoard from '../../actions/board_actions';
 
 class CreateBoard extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      title: props.board ? props.board.title : '',
+    };
+
+  }
+  handleChange(e){
+    this.setState({title: e.currentTarget.value});
   }
 
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.createBoard({title: this.state.title});
+    this.setState({title: ''});
+  }
+
+
   render(){
-    
+    return(
+      <div>
+        <p>Create Board</p>
+        <hr/>
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input value={this.state.title} onChange={this.handleChange.bind(this)} type="text" placeholder="Board Name"/>
+          <input type="submit" value="Create"/>
+        </form>
+      </div>
+    );
   }
 }
+
 
 
 
