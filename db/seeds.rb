88ca448fdccf68afebe8b480219a2ac5a69d4d23
David Veytsman <dveyts@yahoo.com>
2017-11-03 13:35:36 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,19 @@ List.destroy_all
 
 u1 = User.create!(username: "guest", password: "newlife")
 
-b1 = Board.create!(creator_id: u1.id, title: "I am a board")
+b1 = Board.create!(creator_id: u1.id, title: "App Academy Homework")
+b2 = Board.create!(creator_id: u1.id, title: "Full Stack Project")
+b3 = Board.create!(creator_id: u1.id, title: "Shopping")
+b4 = Board.create!(creator_id: u1.id, title: "Job Hunting")
+b5 = Board.create!(creator_id: u1.id, title: "Finances")
 
-l1 = List.create!(creator_id: u1.id , title: "I am a list" ,board_id: b1.id)
-l2 = List.create!(creator_id: u1.id, title: "I am a second list", board_id: b1.id)
+l1 = List.create!(creator_id: u1.id , title: "Alpha course to do list" ,board_id: b1.id)
+l2 = List.create!(creator_id: u1.id , title: "Ruby to do list" ,board_id: b1.id)
+l3 = List.create!(creator_id: u1.id , title: "Rails to do list" ,board_id: b1.id)
+l4 = List.create!(creator_id: u1.id , title: "Relearn rails list" ,board_id: b2.id)
+l5 = List.create!(creator_id: u1.id , title: "Relearn react list" ,board_id: b2.id)
+l6 = List.create!(creator_id: u1.id, title: "Costco list", board_id: b3.id)
+l7 = List.create!(creator_id: u1.id, title: "Trader joes list", board_id: b3.id)
+l8 = List.create!(creator_id: u1.id, title: "Whole foods list", board_id: b3.id)
+l9 = List.create!(creator_id: u1.id, title: "Update resume", board_id: b4.id)
+l10 = List.create!(creator_id: u1.id, title: "finish full stack list", board_id: b4.id)
