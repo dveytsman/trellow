b7b090adc747b7d079b54054a834a534910affb5
David Veytsman <dveyts@yahoo.com>
2018-02-16 12:44:12 -0500

    used the title in the form

--- frontend/components/boards/rename_board.jsx
+++ frontend/components/boards/rename_board.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 class RenameBoard extends React.Component{
   constructor(props){
     super(props);
-
+    this.title = this.props.title;
     this.state = {
       title: props.board ? props.board.title : '',
     };
@@ -25,11 +25,14 @@ class RenameBoard extends React.Component{
 
 
   render(){
+    // debugger
+    console.log('title');
+    console.log(this.props.title);
     return(
       <div className="create_board_form" onClick={(e)=> e.stopPropagation()}>
-        <p className='p'>Rename Board</p>
+        <p className='p'>Current Board Name: {this.props.title}</p>
         <form className='creation-form' onSubmit={this.handleSubmit.bind(this)}>
-          <h3>Title</h3>
+          <h3>New Title</h3>
           <input className='creation-input' value={this.state.title} onChange={this.handleChange.bind(this)} type="text" />
           <input className='creation-submit' type="submit" value="Update"/>
         </form>
