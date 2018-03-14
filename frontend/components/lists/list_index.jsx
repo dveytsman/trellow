fbe5f5cb46ee00f70be734728cfb68accb1321d4
David Veytsman <dveyts@yahoo.com>
2018-03-14 17:46:39 -0400

    working on displaying the update lists

--- frontend/components/lists/list_index.jsx
+++ frontend/components/lists/list_index.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import {fetchBoards, createBoard} from '../../actions/board_actions';
 import { createList } from '../../actions/list_actions';
 import CreateListContainer from '../lists/create_list_container';
+import UpdateListContainer from '../lists/update_list_container';
 import DropdownContainer from '../navbar/dropdown_container';
 
 
 class ListIndex extends React.Component{
   constructor(props){
     super(props);
-
+    this.updateList = this.updateList.bind(this);
     this.deleteList = this.deleteList.bind(this);
   }
 
@@ -21,7 +22,6 @@ class ListIndex extends React.Component{
     };
   }
   componentWillReceiveProps(ownProps){
-    // debugger
 
   }
 
@@ -31,9 +31,13 @@ class ListIndex extends React.Component{
 
   }
 
+  updateList(e, list){
+    e.preventDefault();
+    this.props.updateList(list);
+  }
+
 
   render(){
-// debugger
     var content = (this.props.lists === null) ? [] : this.props.lists;
 
     const listsIndex = content.map(list => {
@@ -43,6 +47,12 @@ class ListIndex extends React.Component{
       return(
         <li key={list.id}
           className="list-items">{list.title}
+          <button className='rename-list-button' onClick={this.toggleDropDown(`update-list-${list.id}`)}>
+            rename
+          </button>
+          <DropdownContainer name={`update-list-${list.id}`}>
+            <UpdateListContainer board={this.props.board} list={list}/>
+          </DropdownContainer>
           <button className='delete-list-button' onClick={(e) => this.deleteList(e, list)}>
             delete
           </button>
@@ -50,17 +60,17 @@ class ListIndex extends React.Component{
       );
     });
     return(
-        <ul>
-          <div  className='list-container'>
-            {listsIndex}
-            <div className="create-list board-items-cont" onClick={this.toggleDropDown('create-list-modal') }>
-              <p id="create-list-text">Create List</p>
-              <DropdownContainer name="create-list-modal">
-                <CreateListContainer board={this.props.board}/>
-              </DropdownContainer>
-            </div>
+      <ul>
+        <div  className='list-container'>
+          {listsIndex}
+          <div className="create-list board-items-cont" onClick={this.toggleDropDown('create-list-modal') }>
+            <p id="create-list-text">Create List</p>
+            <DropdownContainer name="create-list-modal">
+              <CreateListContainer board={this.props.board}/>
+            </DropdownContainer>
           </div>
-        </ul>
+        </div>
+      </ul>
     );
   }
 }
