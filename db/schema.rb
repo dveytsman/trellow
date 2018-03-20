0c260a6a8819d019f5cc37e58ae750a2395999c0
David Veytsman <dveyts@yahoo.com>
2018-03-20 17:03:15 -0400

    added list items to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102224409) do
+ActiveRecord::Schema.define(version: 20180320204919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20171102224409) do
   create_table "boards", force: :cascade do |t|
     t.string "title", null: false
     t.integer "creator_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "list_id", null: false
+    t.string "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
