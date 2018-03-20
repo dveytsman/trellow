8e70e91cdfa37bffc9a69a4ec4b29531d4ca303f
David Veytsman <dveyts@yahoo.com>
2018-03-20 17:30:33 -0400

    added controller for list items

--- app/controllers/api/list_items_controller.rb
+++ app/controllers/api/list_items_controller.rb
@@ -1,2 +1,32 @@
 class Api::ListItemsController < ApplicationController
+  before_action :require_login, only: [:create, :update, :destroy]
+
+  def create
+    @list_item = ListItem.new(list_item_params)
+    if @list_item.save
+      render json: @list_item
+    else
+      render @list_item.errors.full_messages,
+      status: 422
+    end
+  end
+
+  def update
+    @list_item = ListItem.find(params[:id])
+    if @list_item.update(list_item_params)
+      render json: @list_item
+    else
+      render @list_items.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @list_item = ListItem.find(params[:id])
+    @list_item.destroy
+  end 
+
+  def list_item_params
+    params.require(:list_item).permit(:title, :body)
+  end
+
 end
