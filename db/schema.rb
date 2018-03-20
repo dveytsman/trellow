293e4e881172eec5b6b460273fd952730f5924f3
David Veytsman <dveyts@yahoo.com>
2018-03-20 17:31:04 -0400

    changed the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320204919) do
+ActiveRecord::Schema.define(version: 20180320211105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20180320204919) do
     t.string "creator_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
   create_table "lists", force: :cascade do |t|
