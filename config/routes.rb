cea1c79e79ba04a3366febf8588f42c81cbc1972
David Veytsman <dveyts@yahoo.com>
2018-03-27 15:49:04 -0400

    simplified the destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
+    resources :list_items, only: [:destroy]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy]
     resources :boards, only: [:create, :index, :show, :update, :destroy] do
       resources :lists, only: [:create, :update, :destroy] do
-        resources :list_items, only: [:create, :update, :destroy]
+        resources :list_items, only: [:create, :update]
       end
     end
   end
