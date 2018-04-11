e7328b63c74282b469637b562296620ff56cf994
David Veytsman <dveyts@yahoo.com>
2018-04-11 14:21:33 -0400

    added the list id

--- frontend/components/list_items/list_item_index_container.js
+++ frontend/components/list_items/list_item_index_container.js
@@ -3,17 +3,17 @@ import { createListItem, updateListItem, deleteListItem } from '../../actions/li
 import ListItemIndex from './list_item_index';
 
 const mapStateToProps = (state, ownProps) => {
+
   return {
-    board: ownProps.board,
-    lists: (ownProps.board) ? ownProps.board.listIds.sort().map(id=> state.lists[id]) : null
+    list: (ownProps.list)
   };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    createListitem: (listItem) => dispatch(createListItem(listItem)),
+    createListItem: (listItem, boardId) => dispatch(createListItem(listItem, boardId)),
     updateListItem: (listItem) => dispatch(updateListItem(listItem)),
-    deleteListItem: (listItem) => dispatch(deleteListItem(listItem))
+    deleteListItem: (listItem, listId) => dispatch(deleteListItem(listItem, listId))
   };
 };
 
