ac7f10e2f40299beae4140d4d686fae5ed5dfb02
David Veytsman <dveyts@yahoo.com>
2018-04-11 14:15:24 -0400

    fixed the components

--- frontend/components/list_items/list_item_index.jsx
+++ frontend/components/list_items/list_item_index.jsx
@@ -4,19 +4,41 @@ import { Link } from 'react-router-dom';
 class ListItemIndex extends React.Component{
   constructor(props){
     super(props);
-
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.state = {title: ''};
   }
   componentWillReceiveProps(ownProps){
 
   }
+  handleDelete(e){
+    e.preventDefault();
+    this.props.deleteListItem(Number(e.target.id), this.props.list.id);
+  }
+
+  handleChange(e){
+    e.preventDefault();
+    this.setState({title: e.currentTarget.value});
+  }
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.createListItem({title: this.state.title, list_id: this.props.list.id}, this.props.list.board_id);
+    this.setState({title: ''});
+  }
   render(){
-        var cards = (this.props.list.card === undefined) ? [] : Object.values(this.props.list.card);
+        var cards = (this.props.list.cards === undefined) ? [] : Object.values(this.props.list.cards);
     var cardsIndex = cards.map(card => (
-      <li key={card.id} className="card">{card.title}</li>
+      <li onClick={this.handleDelete} key={card.id} className="card">{card.title}<div id={card.id} className="x" >x</div></li>
     ));
     return(
       <ul className="cards">
           {cardsIndex}
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input onChange={this.handleChange} type="text" value={this.state.title}/>
+            </form>
+            </li>
       </ul>
     )
 
