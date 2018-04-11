76aa996ae93b88993cf4187bc836c04b83d9af4d
David Veytsman <dveyts@yahoo.com>
2018-04-11 14:15:01 -0400

    fixed list_item actions

--- frontend/actions/list_item_actions.js
+++ frontend/actions/list_item_actions.js
@@ -3,29 +3,31 @@ import * as ListItemApiUtil from '../util/list_item_api_util';
 export const RECEIVE_LIST_ITEM = "RECEIVE_LIST_ITEM";
 export const REMOVE_LIST_ITEM = "REMOVE_LIST_ITEM";
 
-export const receiveListItem = (payload) => {
+export const receiveListItem = (payload, boardId) => {
   return {
     type: RECEIVE_LIST_ITEM,
-    payload
+    payload,
+    boardId
   };
 };
 
-export const removeListItem = (itemId) => {
+export const removeListItem = (itemId, listId) => {
   return {
     type: REMOVE_LIST_ITEM,
-    itemId
+    itemId,
+    listId
   };
 };
 
-export const createListItem = (listItem) => (dispatch) => {
+export const createListItem = (listItem, boardId) => (dispatch) => {
   return(
-    ListItemApiUtil.createListItem(listItem).then(listItem => dispatch(receiveListItem(listItem)))
+    ListItemApiUtil.createListItem(listItem, boardId).then(listItem => dispatch(receiveListItem(listItem, boardId)))
   );
 };
 
-export const deleteListItem = (itemId) => (dispatch) => {
+export const deleteListItem = (itemId, listId) => (dispatch) => {
   return(
-    ListItemApiUtil.deleteListItem(itemId).then((itemId) => dispatch(removeListItem(itemId)))
+    ListItemApiUtil.deleteListItem(itemId, listId).then(() => dispatch(removeListItem(itemId, listId)))
   );
 };
 
