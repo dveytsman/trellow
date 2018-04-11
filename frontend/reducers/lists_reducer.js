e380f541a90fb2571bea5450aab5de134eadbba0
David Veytsman <dveyts@yahoo.com>
2018-04-11 14:23:04 -0400

    passed in the list Id

--- frontend/reducers/lists_reducer.js
+++ frontend/reducers/lists_reducer.js
@@ -1,19 +1,27 @@
 import { RECEIVE_BOARD } from '../actions/board_actions';
 import { RECEIVE_LIST } from '../actions/list_actions';
 import { REMOVE_LIST } from '../actions/list_actions';
-import merge from 'lodash/merge';
 import { RECEIVE_LIST_ITEM, REMOVE_LIST_ITEM } from '../actions/list_item_actions';
+import merge from 'lodash/merge';
 
 const ListReducer = (oldState={}, action) => {
   Object.freeze(oldState);
+  let newState;
   switch (action.type) {
     case RECEIVE_BOARD:
       return merge({}, oldState, action.payload.lists);
     case RECEIVE_LIST:
       return merge({}, oldState,  {[action.payload.id]:  action.payload});
     case REMOVE_LIST:
-      let newState = merge({}, oldState);
+      newState = merge({}, oldState);
       delete newState[action.listId.id];
+      return newState;
+    case REMOVE_LIST_ITEM:
+      newState = merge({}, oldState);
+      delete newState[action.listId].cards[action.itemId];
+      return newState;
+    case RECEIVE_LIST_ITEM:
+      newState = merge({}, oldState, {[[action.payload.list_id]]:{cards: {[action.payload.id]: action.payload}}});
       return newState;
     default:
       return oldState;
